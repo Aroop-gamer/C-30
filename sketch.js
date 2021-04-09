794d3eca9d929236107ec960e6c3dc90c4836374
Aroop-gamer <aroopb107@gmail.com>
2021-04-09 13:45:44 +0530

    vanishing pigs

--- sketch.js
+++ sketch.js
@@ -75,3 +75,39 @@ function mouseDragged(){
 function mouseReleased(){
     slingshot.fly();
 }
+
+
+function keyPressed(){
+
+    if(keyCode===32){
+
+    slingshot.attach(bird.body)
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
